8a16ee6ab080af39cb1b736f7962ef1d476164da
HuiGeYang <516000597@qq.com>
2016-02-17 14:12:16 +0800

    Create we.js

---
+++ we.js
@@ -0,0 +1,18 @@
+//加载sea.js
+(function(m, o, d, u, l, a, r) {
+	if(m[d]){return;}
+	function f(n, t) {//闭包
+		return function() {
+			r.push(n, arguments);
+			return t;
+		};
+	}
+	m[d] = a = { args: (r = []), config: f(0, a), use: f(1, a) };
+	m.define = f(2);
+	u = o.createElement('script');
+	u.id = d + 'node';
+	u.src = we.basepath+'sea.js';
+	l = o.getElementsByTagName('head')[0];
+	a = o.getElementsByTagName('base')[0];
+	a ? l.insertBefore(u, a) : l.appendChild(u);
+})(window, document, 'seajs');
