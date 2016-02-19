53e82a903ef55de9840d7f5c638b79b9325a14e4
prettyfu <confidencefnt@163.com>
2016-02-19 15:23:20 +0800

    test commit

--- we.js
+++ we.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+//test git
 (function(){
 	var we = {};
 	we.version  = '1.0';
@@ -24,23 +24,3 @@
 	})(window, document, 'seajs');
 
 })();
-=======
-//加载sea.js
-(function(m, o, d, u, l, a, r) {
-	if(m[d]){return;}
-	function f(n, t) {//闭包
-		return function() {
-			r.push(n, arguments);
-			return t;
-		};
-	}
-	m[d] = a = { args: (r = []), config: f(0, a), use: f(1, a) };
-	m.define = f(2);
-	u = o.createElement('script');
-	u.id = d + 'node';
-	u.src = we.basepath+'sea.js';
-	l = o.getElementsByTagName('head')[0];
-	a = o.getElementsByTagName('base')[0];
-	a ? l.insertBefore(u, a) : l.appendChild(u);
-})(window, document, 'seajs');
->>>>>>> 8a16ee6ab080af39cb1b736f7962ef1d476164da
